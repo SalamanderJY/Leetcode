9815aa7c1e9489bf682a0b779217784e9c9b2cc5
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2016-03-18 15:03:15 +0800

    add

---
+++ _21_Merge_Two_Sorted_Lists/Solution.java
@@ -0,0 +1,43 @@
+package _21_Merge_Two_Sorted_Lists;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) 
+          return l2;
+        if(l2 == null)
+          return l1;
+          
+        ListNode head = new ListNode(0);
+        head.next = null; 
+        ListNode temp = head;
+          
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }
+            else {
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+        }
+        
+        if(l1 != null) {
+            temp.next = l1;
+        }
+        
+        else if(l2 != null) {
+            temp.next = l2;
+        }
+        
+        return head.next;
+    }
+}
