b5098967ab75b22ae2a2c4347731b3301c0fe081
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2016-04-08 00:05:32 +0800

    HashMap

---
+++ _205_Isomorphic_Strings/Solution.java
@@ -0,0 +1,38 @@
+package _205_Isomorphic_Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public boolean isIsomorphic(String s, String t) {
+		char[] strs = s.toCharArray();
+		char[] strt = t.toCharArray();
+		
+		Map<Character, Character> mapkey = new HashMap<Character, Character>();
+		Map<Character, Character> mapvalue = new HashMap<Character, Character>();
+    	boolean flag = true;
+    	for(int i = 0; i < strs.length; i++) {
+    		if( (!mapkey.containsKey(strs[i])) && (!mapvalue.containsKey(strt[i])) ){
+    			mapkey.put(strs[i], strt[i]);
+    			mapvalue.put(strt[i], strs[i]);
+    		} else {
+    			if(mapkey.containsKey(strs[i])){
+	    			if(mapkey.get(strs[i]) == strt[i]){
+	    				continue;
+	    			} else {
+	    				flag = false;
+	    				break;
+	    			}
+    			} else {
+    				if(mapvalue.get(strt[i]) == strs[i]){
+	    				continue;
+	    			} else {
+	    				flag = false;
+	    				break;
+	    			}
+    			}
+    		}
+    	}  	
+    	return flag;        
+    }
+}
