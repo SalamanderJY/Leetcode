4566479c7f20301663ec65d33a8260997be376e2
Jianyu.Wang <873869027@qq.com>
2018-01-24 20:37:21 +0800

    cpp 001 two sum

---
+++ Cplusplus/_1_Two_Sum/Solution_001.cpp
@@ -0,0 +1,31 @@
+#include <iostream>  
+#include <unordered_map>  
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+
+		unordered_map<int, int> hash;
+		vector<int> result;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int numToFind = target - nums[i];
+
+			if (hash.find(numToFind) != hash.end())
+			{
+				result.push_back(hash[numToFind]);
+				result.push_back(i);
+				return result;
+			}
+
+			hash[nums[i]] = i;
+		}
+
+		return result;
+
+	}
+};
