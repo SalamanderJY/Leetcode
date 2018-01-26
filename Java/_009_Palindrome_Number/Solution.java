55045fbded5243cd5f34ce2483342d3fe14d9ae9
ubuntu <873869027@qq.com>
2018-01-26 15:55:09 +0800

    009 Palindrome Number

--- Java/_009_Palindrome_Number/Solution.java
+++ Java/_009_Palindrome_Number/Solution.java
@@ -2,7 +2,7 @@
 
 public class Solution {
 	public boolean isPalindrome(int x) {
-        if( x<0 || (x != 0 && x%10 == 0) )
+        if( x < 0 || (x != 0 && x % 10 == 0) )
           return false;
         int res = 0;
         while(x > res) {
---
+++ Python/_009_Palindrome_Number/solution.py
@@ -0,0 +1,7 @@
+def isPalindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    return str(x)[::-1] == str(x)
+
