2a901a1d82f6a7196b9175463c3e85e7219ed868
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-01-26 15:55:09 +0800

    009 Palindrome Number

---
+++ Cplusplus/_009_Palindrome_Number/Solution_009.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x != 0 && x % 10 == 0))
+          return false;
+        int res = 0;
+        while(x > res) 
+        {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        // 返回偶数回文或奇数回文
+        return (x == res || x == res/10);
+    }
+};
