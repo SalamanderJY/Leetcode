4098369fc7056f8b798ab773a36a1bdb99dc8228
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-02-03 20:16:54 +0800

    038 Count and Say

---
+++ Cplusplus/_038_Count_and_Say/Solution_038.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+	string countAndSay(int n) {
+		if (n < 1)
+			return NULL;
+
+		string result = "1";
+
+		for (int i = 2; i <= n; i++)
+			result = countAndSay(result);
+        
+		return result;
+	}
+
+	string countAndSay(string str)
+	{
+		string result = "";
+		int count = 1;
+		for (int i = 1; i < str.size(); i++)
+		{
+			if (str.at(i) == str.at(i - 1))
+				count++;
+			else
+			{
+				result += to_string(count);
+				result += str.at(i - 1);
+				count = 1;
+			}
+		}
+		result += to_string(count);
+		result += str.at(str.size() - 1);
+		return result;
+	}
+};
---
+++ Python/_038_Count_and_Say/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n < 1:
+            return None   
+        result = "1"     
+        for i in range(2, n + 1):
+            result = countRecurrent(result)    
+        return result
+
+def countRecurrent(string):
+        result = ""
+        count = 1
+        for i in range(1, len(string)):
+            if string[i] == string[i-1]:
+                count += 1
+            else:
+                result += str(count)
+                result += string[i-1]
+                count = 1
+        result += str(count)
+        result += string[len(string) - 1]
+        return result
