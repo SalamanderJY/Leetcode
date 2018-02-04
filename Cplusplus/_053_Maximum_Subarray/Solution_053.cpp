3f9821576128975a94907cb759f7d233dbef6503
ubuntu <873869027@qq.com>
2018-02-04 14:26:36 +0800

    053 Maximum Subarray

---
+++ Cplusplus/_053_Maximum_Subarray/Solution_053.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maximum = nums[0];
+        int* dp = new int[nums.size()];
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp[i] = dp[i-1] > 0 ? dp[i-1] + nums[i] : nums[i];
+            maximum = max(maximum, dp[i]);
+        }
+        
+        return maximum;
+    }
+};
+
