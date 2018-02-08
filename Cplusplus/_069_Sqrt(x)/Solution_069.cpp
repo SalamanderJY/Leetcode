d3d1aaf1386cfe1823d7b10e95e472792c4d265f
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-02-08 14:34:40 +0800

    069 Sqrt(x)

---
+++ Cplusplus/_069_Sqrt(x)/Solution_069.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        unsigned int result = 0;
+        for(int i = 15; i >= 0; i--)
+        {
+            if((result + (1 << i)) * (result + (1 << i)) <= x)
+                result += (1 << i);
+        }
+        return result;
+    }
+};
