9d0a94d8648565fc71d8d2ce2c5757b91a0941f6
Jianyu.Wang <873869027@qq.com>
2018-02-11 15:00:28 +0800

    088 Merge Sorted Array

---
+++ Cplusplus/_088_Merge_Sorted_Array/Solution_088.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int sum = m + n - 1;
+        int mm = m - 1;
+        int nn = n - 1;
+        while(nn >= 0 && mm >= 0)
+        {
+            if(nums1[mm] <= nums2[nn])
+            {
+                nums1[sum] = nums2[nn];
+                sum--;
+                nn--;
+            }
+            else
+            {
+                nums1[sum] = nums1[mm];
+                sum--;
+                mm--;
+            }
+        }
+        while(nn >= 0)
+        {
+            nums1[sum] = nums2[nn];
+            sum--;
+            nn--;
+        }
+    }
+};
