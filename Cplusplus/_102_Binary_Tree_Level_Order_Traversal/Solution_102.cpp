6d0732242e5630fb70d321b44a9e1ecff0cd9d13
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-02-17 19:38:38 +0800

    Update Solution_102.cpp
    format 102 Solution.

--- Cplusplus/_102_Binary_Tree_Level_Order_Traversal/Solution_102.cpp
+++ Cplusplus/_102_Binary_Tree_Level_Order_Traversal/Solution_102.cpp
@@ -11,26 +11,26 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> result;
-		vector<TreeNode*> level;
-		level.push_back(root);
-		while (root != NULL && level.size() > 0)
-		{
-			vector<int> cur_level;
-			for (int i = 0; i < level.size(); i++)
-				cur_level.push_back(level.at(i)->val);
-			if (cur_level.size() > 0)
-				result.push_back(cur_level);
+	vector<TreeNode*> level;
+	level.push_back(root);
+	while (root != NULL && level.size() > 0)
+	{
+		vector<int> cur_level;
+		for (int i = 0; i < level.size(); i++)
+			cur_level.push_back(level.at(i)->val);
+		if (cur_level.size() > 0)
+			result.push_back(cur_level);
 
-			vector<TreeNode*> next_level;
-			for (int i = 0; i < level.size(); i++)
-			{
-				if (level.at(i)->left != NULL)
-					next_level.push_back(level.at(i)->left);
-				if (level.at(i)->right != NULL)
-					next_level.push_back(level.at(i)->right);
-			}
-			level = next_level;
+		vector<TreeNode*> next_level;
+		for (int i = 0; i < level.size(); i++)
+		{
+			if (level.at(i)->left != NULL)
+				next_level.push_back(level.at(i)->left);
+			if (level.at(i)->right != NULL)
+				next_level.push_back(level.at(i)->right);
 		}
-		return result;
+		level = next_level;
+	}
+	return result;
     }
 };
