f5ecefb3078e03d673ad5d03add47c50c45c57e5
Jianyu.Wang <873869027@qq.com>
2018-02-23 00:13:03 +0800

    168 Excel Sheet Column Title

---
+++ Java/_168_Excel_Sheet_Column_Title/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int n) {
+        String res = "";
+        while (n > 0) {
+            n--;
+            res = (char)(n % 26 + 65) + res;
+            n /= 26;
+        }
+        return res;
+    }
+}
