28db1a4339ab3adf69fd06aebaab4a9061e88e96
Jianyu.Wang <873869027@qq.com>
2018-03-16 23:49:06 +0800

    389 Find the Difference

---
+++ Python/_389_Find_the_Difference/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        ans = 0
+        for c in s + t:
+            ans ^= ord(c)
+        return chr(ans)
