862215ef1996dc44353974b9d6b3e664921eb21c
Jianyu.Wang <873869027@qq.com>
2018-03-31 10:53:36 +0800

    461 Hamming Distance

---
+++ Cplusplus/_461_Hamming_Distance/Solution_461.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int diff = x ^ y;
+        int sum = 0;
+        while(diff > 0) {
+            sum += diff & 1;
+            diff >>= 1;
+        }
+        return sum;
+    }
+};
