38c6b3440f6207d1cf4bf0f6aa81f7a0da42a6ab
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-04-09 21:01:36 +0800

    006 ZigZag Conversion

--- Java/_006_ZigZag_Conversion/Solution.java
+++ Java/_006_ZigZag_Conversion/Solution.java
@@ -1,30 +1,25 @@
-package _6_ZigZag_Conversion;
-
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public String convert(String s, int numRows) {
-        if(numRows == 1)
-          return s;
+        if (numRows == 1)
+            return s;
         String[] result = new String[numRows];
         Arrays.fill(result, "");  
         char[] source = s.toCharArray();
         int i = 0;
         int j = 0;
         int back = numRows - 2;
-        while(i < s.length()) {
-            for(j = 0; j < s.length() && j < numRows && i < s.length(); j++) {
+        while (i < s.length()) {
+            for (j = 0; j < s.length() && j < numRows && i < s.length(); j++) {
                 result[j] += source[i];
                 i++;
             }
-            for(j = back; j < s.length() && j > 0 && i < s.length(); j--) {
+            for (j = back; j < s.length() && j > 0 && i < s.length(); j--) {
                 result[j] += source[i];
                 i++;
             }
         }
-
-        for(i = 0; i < numRows - 1; i++)
-          result[0] += result[i+1];
+        for (i = 0; i < numRows - 1; i++)
+            result[0] += result[i + 1];
         
         return result[0];
     }
