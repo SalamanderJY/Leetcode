fea711496cda1b7f57521fb3711ef3a8505571d3
ubuntu <873869027@qq.com>
2018-04-24 16:43:14 +0800

    018 4Sum

---
+++ Java/_018_4Sum/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums.length == 0)
+            return res;
+        // sort the array
+        Arrays.sort(nums);
+        // hash map
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++)
+            map.put(nums[i], i);
+        
+        for (int i = 0; i < nums.length - 3; i++)
+        {
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) 
+                return res;
+            if (nums[i] + nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3] < target)
+                continue;    
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+            for (int j = i + 1; j < nums.length - 2; j++)
+            {
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target)
+                    break;
+                if (nums[i] + nums[j] + nums[nums.length - 1] + nums[nums.length - 2] < target)
+                    continue;
+                if (j - 1 > i && nums[j] == nums[j - 1]) 
+                    continue;
+                for (int k = j + 1; k < nums.length - 1; k++)
+                {
+                    if (k - 1 > j && nums[k] == nums[k - 1])
+                        continue;
+                    int three = nums[i] + nums[j] + nums[k];
+                    if (map.containsKey(target - three) && map.get(target - three) > k)
+                    {
+                        List<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(target - three);
+                        res.add(temp);
+                    }   
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ Python/_018_4Sum/solution.py
@@ -0,0 +1,42 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        if len(nums) == 0:
+            return res
+        nums = sorted(nums)
+        mapping = {}
+        for i in range(0, len(nums)):
+            mapping[nums[i]] = i
+        
+        for i in range(0, len(nums) - 3):
+            if nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target: 
+                return res
+            if nums[i] + nums[len(nums) - 1] + nums[len(nums) - 2] + nums[len(nums) - 3] < target:
+                continue
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target:
+                    break
+                if nums[i] + nums[j] + nums[len(nums) - 1] + nums[len(nums) - 2] < target:
+                    continue
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue   
+                for k in range(j + 1, len(nums) - 1):
+                    if k - 1 > j and nums[k] == nums[k - 1]:
+                        continue
+                    three = nums[i] + nums[j] + nums[k]
+                    # exists and not traverse
+                    if target - three in mapping and mapping[target - three] > k:
+                        temp = []
+                        temp.append(nums[i])
+                        temp.append(nums[j])
+                        temp.append(nums[k])
+                        temp.append(target - three)
+                        res.append(temp)
+        return res
