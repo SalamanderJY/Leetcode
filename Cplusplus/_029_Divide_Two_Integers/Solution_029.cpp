2a5c2594fec6ff639b20417bca05196d797d1307
Jianyu.Wang <873869027@qq.com>
2018-04-28 23:46:38 +0800

    029 Divide Two Integers

---
+++ Cplusplus/_029_Divide_Two_Integers/Solution_029.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+        long res = 0;
+        long m = abs((long)dividend);
+        long n = abs((long)divisor);
+        while (m >= n)
+        {
+            long temp = n, i = 1;
+            while (temp << 1 < m)
+            {
+                temp <<= 1;
+                i <<= 1;
+            }
+            m -= temp;
+            res += i;
+        }
+        if (sign < 0)
+            res = -res;
+        return res > INT_MAX ? INT_MAX : res;
+    }
+};
