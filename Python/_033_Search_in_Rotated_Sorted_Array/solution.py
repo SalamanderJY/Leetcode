a4594795ab241a1cb460e18b19390373a8e14df1
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-04-30 22:20:24 +0800

    033 Search in Rotated Sorted Array

---
+++ Python/_033_Search_in_Rotated_Sorted_Array/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        while left + 1 < right:
+            if nums[left] == target: return left
+            if nums[right] == target: return right
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                if (target < nums[right]) or (target > nums[right] and nums[mid] > nums[right]):
+                    left = mid
+                else: right = mid
+            elif nums[mid] > target:
+                if (target > nums[left]) or (target < nums[left] and nums[mid] < nums[left]):
+                    right = mid
+                else: left = mid
+        if nums[left] == target: return left
+        if nums[right] == target: return right
+        return -1
