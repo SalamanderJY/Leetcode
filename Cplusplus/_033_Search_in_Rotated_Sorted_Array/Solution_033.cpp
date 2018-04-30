68f1d51cbde04bdf06afa87aa6f554b41b50aefc
Jianyu.Wang <873869027@qq.com>
2018-04-30 22:20:24 +0800

    033 Search in Rotated Sorted Array

---
+++ Cplusplus/_033_Search_in_Rotated_Sorted_Array/Solution_033.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 0)
+            return -1;
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left + 1 < right)
+        {
+            if (nums[left] == target) return left;
+            if (nums[right] == target) return right;
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target)
+            {
+                if ((target < nums[right]) || (target > nums[right] && nums[mid] > nums[right]))
+                    left = mid;
+                else right = mid;
+            }
+            else if (nums[mid] > target)
+            {
+                if ((target > nums[left]) || (target < nums[left] && nums[mid] < nums[left]))
+                    right = mid;
+                else left = mid;
+            }
+        }
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
+        return -1;
+    }
+};
