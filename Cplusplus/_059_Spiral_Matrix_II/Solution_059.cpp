119bb2486cd23b854e1311017bf560643a86a7ed
ubuntu <873869027@qq.com>
2018-05-16 15:50:35 +0800

    059 Spiral Matrix II

---
+++ Cplusplus/_059_Spiral_Matrix_II/Solution_059.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> result(n, vector<int>(n));
+        int count = 1;
+        int begin = 0;
+        while (count <= n * n) {
+            for (int i = begin; i < n - begin; i++) {
+                result[begin][i] = count;
+                count++;
+                if(count > n * n)
+                  return result;
+            }
+            count--;
+            for (int i = begin; i < n - begin; i++) {
+                result[i][n - 1 - begin] = count;
+                count++;
+                if(count > n * n)
+                  return result;
+            }
+            count--;
+            for (int i = n - 1 - begin; i >= begin; i--) {
+                result[n - 1 - begin][i] = count;
+                count++;
+                if(count > n * n)
+                  return result;
+            }
+            count--;
+            for (int i = n - 1 - begin; i > begin; i--) {
+                result[i][begin] = count;
+                count++;
+                if (count > n * n)
+                  return result;
+            }
+            begin++;
+        }
+        return result;
+    }
+};
