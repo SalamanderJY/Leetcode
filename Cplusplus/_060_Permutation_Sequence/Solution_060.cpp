e44609dc49c0a2d8a991a467cf0b19e38e3eefbc
Jianyu.Wang <873869027@qq.com>
2018-05-19 19:25:26 +0800

    060 Permutation Sequence

---
+++ Cplusplus/_060_Permutation_Sequence/Solution_060.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string res;
+        string num = "123456789";
+        vector<int> factorial(n, 1);
+        for (int i = 1; i < n; ++i) 
+            factorial[i] = factorial[i - 1] * i;
+        k--;
+        for (int i = n; i >= 1; --i) {
+            int j = k / factorial[i - 1];
+            k %= factorial[i - 1];
+            res.push_back(num[j]);
+            num.erase(j, 1);
+        }
+        return res;
+    }
+};
