eb4ad592bb635f758a95b7e78fcc9751ed23b3cc
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-05-22 17:50:23 +0800

    071 Simplify Path

---
+++ Cplusplus/_071_Simplify_Path/Solution_071.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string res, tmp;
+        vector<string> stack;
+        stringstream ss(path);
+        while(getline(ss, tmp, '/')) {
+            if (tmp == "" || tmp == ".") continue;
+            if (tmp == ".." && !stack.empty()) stack.pop_back();
+            else if (tmp != "..") stack.push_back(tmp);
+        }
+        for(auto str : stack) res += "/" + str;
+        return res.empty() ? "/" : res;
+    }
+};
