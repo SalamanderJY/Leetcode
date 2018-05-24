5d72dd7d9b165113b7c24a3a65443e5c84a92f40
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-05-24 15:22:16 +0800

    074 Search a 2D Matrix

---
+++ Python/_074_Search_a_2D_Matrix/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        i = len(matrix) - 1
+        j = 0
+        while i >= 0 and j <= len(matrix[0]) - 1:
+            if target > matrix[i][j]:
+                j += 1
+            elif target < matrix[i][j]:
+                i -= 1;
+            else:
+                return True
+        return False
