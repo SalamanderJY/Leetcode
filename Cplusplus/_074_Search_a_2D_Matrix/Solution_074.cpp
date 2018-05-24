c19cd9cbdae16a7b6e604154cd525c0de8fe791e
Jianyu.Wang <873869027@qq.com>
2018-05-24 15:22:16 +0800

    074 Search a 2D Matrix

---
+++ Cplusplus/_074_Search_a_2D_Matrix/Solution_074.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        int i = matrix.size() - 1;
+        int j = 0;
+        while (i >= 0 && j <= matrix[0].size() - 1) {
+            if (target > matrix[i][j])
+                ++j;
+            else if (target < matrix[i][j])
+                --i;
+            else
+                return true;
+        }
+        return false;
+    }
+};
---
+++ Java/_074_Search_a_2D_Matrix/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >= 0 && j <= matrix[0].length - 1) {
+            if (target > matrix[i][j])
+                ++j;
+            else if (target < matrix[i][j])
+                --i;
+            else
+                return true;
+        }
+        return false;
+    }
+}
