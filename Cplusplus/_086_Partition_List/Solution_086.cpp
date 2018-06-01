c50cbc83b1c4a8ba229fb4cf4d6f5c4ab203a7bd
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-06-01 23:31:00 +0800

    086 Partition List

---
+++ Cplusplus/_086_Partition_List/Solution_086.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode* small = new ListNode(0);
+        ListNode* ts = small;  
+        ListNode* large = new ListNode(0);
+        ListNode* tl = large;
+        while (head != 0) {
+            if (head->val < x) {
+                ts->next = head;
+                ts = ts->next;
+            }
+            else {
+                tl->next = head;
+                tl = tl->next;
+            }
+            head = head->next;
+        }
+        tl->next = NULL; 
+        ts->next = large->next; 
+        return small->next;
+    }
+};
