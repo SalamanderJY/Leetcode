15d8d96a348bc6d320c394d98b6867a858f50d2f
Jianyu.Wang <873869027@qq.com>
2018-06-02 14:34:33 +0800

    089 Gray Code

---
+++ Cplusplus/_089_Gray_Code/Solution_089.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        for (int i = 0; i < (int)pow(2, n); ++i)
+            result.push_back(i ^ (i >> 1));
+        return result;
+    }
+};
