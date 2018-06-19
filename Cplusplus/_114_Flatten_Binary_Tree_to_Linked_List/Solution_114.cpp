c30a0b5f802c667d1399b0a2887755383363530d
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-06-19 22:36:21 +0800

    114 Flatten Binary Tree to Linked List

---
+++ Cplusplus/_114_Flatten_Binary_Tree_to_Linked_List/Solution_114.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left) {
+                TreeNode* temp = curr->left;
+                while (temp->right)
+                    temp = temp->right;
+                temp->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
