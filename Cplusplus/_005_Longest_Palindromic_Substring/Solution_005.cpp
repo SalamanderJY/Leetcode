aae17069048aca6b7140c2121fc1e0c22df63559
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-01 17:29:51 +0800

    Update Solution_005.cpp

--- Cplusplus/_005_Longest_Palindromic_Substring/Solution_005.cpp
+++ Cplusplus/_005_Longest_Palindromic_Substring/Solution_005.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         if (s.size() == 0)
-			return s;
-		bool dp[1000][1000] = { false };
-		int start = 0, end = 0;
-		int max = 0;
-        string res;
-		for (int i = 0; i < s.size(); i++)
-		{
-			for (int j = 0; j <= i; j++)
-			{
-				if (s.at(i) == s.at(j) && (i - j <= 2 || dp[j + 1][i - 1]))
-					dp[j][i] = true;
-				if (dp[j][i] && max < i - j + 1)
-				{
-					max = i - j + 1;
-					start = j;
-				}
-			}
+	    return s;
+	    bool dp[1000][1000] = { false };
+	    int start = 0, end = 0;
+	    int max = 0;
+            string res;
+	    for (int i = 0; i < s.size(); i++) {
+		for (int j = 0; j <= i; j++) {
+		    if (s.at(i) == s.at(j) && (i - j <= 2 || dp[j + 1][i - 1]))
+			dp[j][i] = true;
+		    if (dp[j][i] && max < i - j + 1) {
+			max = i - j + 1;
+			start = j;
+		    }
 		}
+	    }
         for (int i = start; i < start + max; i++)
             res += s[i];
-		return res;
+	return res;
         // return s.substr(start, start + max); fuck wrong answer
     }
 };
