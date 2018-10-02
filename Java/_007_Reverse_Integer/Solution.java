2a184a72e6c30e3f468ff915f5f2fcb82cb0a62c
Jianyu.Wang <873869027@qq.com>
2018-10-02 11:48:38 +0800

    Update Solution.java

--- Java/_007_Reverse_Integer/Solution.java
+++ Java/_007_Reverse_Integer/Solution.java
@@ -1,24 +1,12 @@
-package _7_Reverse_Integer;
-
 public class Solution {
-	
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		int i = solution.reverse(115);
-		System.out.println(i);
-	}
-	
     public int reverse(int x) {
-    	 int rev = 0;
-		 while(x!=0)
-		 {
-			 // judge overflow
-			 if (rev != 0 && Integer.MAX_VALUE / rev < 10 && Integer.MAX_VALUE / rev > -10)
-	            return 0;
-			 rev = rev * 10 + x % 10;
-			 x = x / 10;
-		 }
-		 
-		 return rev;	
+    	int rev = 0;
+        while (x != 0) {
+	    if (rev != 0 && Integer.MAX_VALUE / rev < 10 && Integer.MAX_VALUE / rev > -10)
+	        return 0;
+	    rev = rev * 10 + x % 10;
+	    x = x / 10;
+        }
+        return rev;	
     }
 }
