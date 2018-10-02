aa49b61f761e83c8f65921dd9e627fa5cf4cac4f
Jianyu.Wang <873869027@qq.com>
2018-10-02 12:01:02 +0800

    Update Solution.java

--- Java/_008_String_to_Integer/Solution.java
+++ Java/_008_String_to_Integer/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int myAtoi(String str) {
-        if(str.length() == 0) 
-          return 0;       
+        if (str.length() == 0) 
+            return 0;       
         int n = str.length();
         char[] array = str.toCharArray();
         int result = 0;
@@ -17,13 +17,13 @@ class Solution {
         	flag = 1;
         	index++;
         }       
-        for(int i = index; i < n; i++) {
+        for (int i = index; i < n; i++) {
             if (array[i] < '0' || array[i] > '9')
                 break;
             if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && (array[i] - '0') > Integer.MAX_VALUE % 10))
                 return flag == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             result = result * 10 + (array[i] - '0');
         }     
-		return result * flag;
+	return result * flag;
     }
 }
