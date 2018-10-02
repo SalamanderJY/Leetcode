63f94aef4ef19807581b9f5f9617831c32ef6b87
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-02 11:45:33 +0800

    Update Solution_007.cpp

--- Cplusplus/_007_Reverse_Integer/Solution_007.cpp
+++ Cplusplus/_007_Reverse_Integer/Solution_007.cpp
@@ -3,15 +3,12 @@ public:
     int reverse(int x) {
         
         int rev = 0;
-        while (x != 0)
-        {
-            if(rev != 0 && INT_MAX / rev < 10 && INT_MAX / rev > -10)
+        while (x != 0) {
+            if (rev != 0 && INT_MAX / rev < 10 && INT_MAX / rev > -10)
                 return 0;
             rev = rev * 10 + x % 10;
             x = x / 10;
         }
-        
-        return rev;
-              
+        return rev;      
     }
 };
