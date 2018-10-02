ce685fa5cd53a88155f60fa60fe4adea245e75b6
Jianyu.Wang <873869027@qq.com>
2018-10-02 11:48:51 +0800

    Update Solution.java

--- Java/_007_Reverse_Integer/Solution.java
+++ Java/_007_Reverse_Integer/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int reverse(int x) {
-    	int rev = 0;
+    	 int rev = 0;
         while (x != 0) {
 	    if (rev != 0 && Integer.MAX_VALUE / rev < 10 && Integer.MAX_VALUE / rev > -10)
 	        return 0;
