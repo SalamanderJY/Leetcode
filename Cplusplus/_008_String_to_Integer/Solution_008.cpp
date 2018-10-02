5f4566f397bb0777f1f4a8f527cdee7c43600fc8
Jianyu.Wang <873869027@qq.com>
2018-10-02 11:59:46 +0800

    Update Solution_008.cpp

--- Cplusplus/_008_String_to_Integer/Solution_008.cpp
+++ Cplusplus/_008_String_to_Integer/Solution_008.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int myAtoi(string str) {
         if(str.size() == 0) 
-          return 0;       
+            return 0;       
         int result = 0;
         int index = 0;
         int flag = 1;      
@@ -23,6 +23,6 @@ public:
                 return flag == -1 ? INT_MIN : INT_MAX;
             result = result * 10 + (str[i] - '0');
         }     
-		return result * flag;
+	return result * flag;
     }
 };
