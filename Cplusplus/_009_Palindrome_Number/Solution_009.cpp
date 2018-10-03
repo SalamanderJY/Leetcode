628e34a79f5b4d78fb7cb02231e81163777b565f
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-03 23:07:00 +0800

    Update Solution_009.cpp

--- Cplusplus/_009_Palindrome_Number/Solution_009.cpp
+++ Cplusplus/_009_Palindrome_Number/Solution_009.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0 || (x != 0 && x % 10 == 0))
-          return false;
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
         int res = 0;
-        while(x > res) 
-        {
+        while (x > res) {
             res = res * 10 + x % 10;
             x = x / 10;
         }
         // 返回偶数回文或奇数回文
-        return (x == res || x == res/10);
+        return (x == res || x == res / 10);
     }
 };
