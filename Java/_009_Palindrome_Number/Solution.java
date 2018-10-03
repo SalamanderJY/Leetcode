a237fc3176752744d3c447fbcbbcfae9af8876e0
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-03 23:15:01 +0800

    Update Solution.java

--- Java/_009_Palindrome_Number/Solution.java
+++ Java/_009_Palindrome_Number/Solution.java
@@ -1,15 +1,13 @@
-﻿package _9_Palindrome_Number;
-
 public class Solution {
-	public boolean isPalindrome(int x) {
-        if( x < 0 || (x != 0 && x % 10 == 0) )
-          return false;
+	public boolean isPalindrome (int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
         int res = 0;
-        while(x > res) {
-            res = res*10 + x%10;
-            x = x/10;
+        while (x > res) {
+            res = res * 10 + x % 10;
+            x = x / 10;
         }
         // 返回偶数回文或奇数回文
-        return (x == res || x == res/10);
+        return (x == res || x == res / 10);
     }
 }
