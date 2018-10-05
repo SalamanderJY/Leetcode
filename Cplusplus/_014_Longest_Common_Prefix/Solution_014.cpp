401725ccd660d73c3be6011df7118379d94f6074
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-05 12:51:07 +0800

    Update Solution_014.cpp

--- Cplusplus/_014_Longest_Common_Prefix/Solution_014.cpp
+++ Cplusplus/_014_Longest_Common_Prefix/Solution_014.cpp
@@ -5,17 +5,15 @@ using namespace std;
 
 class Solution {
 public:
-	string longestCommonPrefix(vector<string>& strs) {
-		int min = 0;
-		while (strs.size() > 0)
-		{
-			for (string s : strs)
-			{
-				if (s.size() == min || s.at(min) != strs[0].at(min))
-					return strs[0].substr(0, min);
-			}
-			min++;
+    string longestCommonPrefix(vector<string>& strs) {
+	int min = 0;
+	while (strs.size() > 0) {
+	    for (string s : strs) {
+		if (s.size() == min || s.at(min) != strs[0].at(min))
+		    return strs[0].substr(0, min);
 		}
-		return "";
+	    min++;
 	}
+	return "";
+    }
 };
