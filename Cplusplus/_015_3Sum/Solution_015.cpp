72cf53543be3562d82d1a717a600aef547093d61
Jianyu.Wang <873869027@qq.com>
2018-10-05 14:17:53 +0800

    Update Solution_015.cpp

--- Cplusplus/_015_3Sum/Solution_015.cpp
+++ Cplusplus/_015_3Sum/Solution_015.cpp
@@ -11,20 +11,22 @@ public:
         for (int i = 0; i < nums.size(); i++)
             map[nums[i]] = i;
         
-        for (int i = 0; i < nums.size() - 2; i++)
-        {
-            if (nums[i] > 0) return res;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < nums.size() - 1; j++)
-            {
-                if (j - 1 > i && nums[j] == nums[j - 1]) continue;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (nums[i] > 0) 
+                return res;
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+            for (int j = i + 1; j < nums.size() - 1; j++) {
+                if (j - 1 > i && nums[j] == nums[j - 1]) 
+                    continue;
                 int two = nums[i] + nums[j];
-                if (-two < nums[j]) break;
-                if (two > 0) return res;
+                if (-two < nums[j]) 
+                    break;
+                if (two > 0) 
+                    return res;
                 auto it = map.find(-two);
                 // exists and not traverse
-                if (it != map.end() && it->second > j)
-                {
+                if (it != map.end() && it->second > j) {
                     vector<int> temp;
                     temp.push_back(nums[i]);
                     temp.push_back(nums[j]);
