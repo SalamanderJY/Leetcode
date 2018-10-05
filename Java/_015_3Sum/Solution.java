76249fa4dfaa0fc19fe99eb1e4b157d108ebdbe6
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-05 14:21:33 +0800

    Update Solution.java

--- Java/_015_3Sum/Solution.java
+++ Java/_015_3Sum/Solution.java
@@ -10,19 +10,21 @@ class Solution {
         for (int i = 0; i < nums.length; i++)
             map.put(nums[i], i);
         
-        for (int i = 0; i < nums.length - 2; i++)
-        {
-            if (nums[i] > 0) return res;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < nums.length - 1; j++)
-            {
-                if (j - 1 > i && nums[j] == nums[j - 1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) 
+                return res;
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j - 1 > i && nums[j] == nums[j - 1]) 
+                    continue;
                 int two = nums[i] + nums[j];
-                if (-two < nums[j]) break;
-                if (two > 0) return res;
+                if (-two < nums[j]) 
+                    break;
+                if (two > 0) 
+                    return res;
                 // exists and not traverse
-                if (map.containsKey(-two) && map.get(-two) > j)
-                {
+                if (map.containsKey(-two) && map.get(-two) > j) {
                     List<Integer> temp = new ArrayList<Integer>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
