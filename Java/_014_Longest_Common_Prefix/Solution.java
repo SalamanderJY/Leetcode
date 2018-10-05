faef389b518bf08c8c5e472e5912a2852665224b
Jianyu.Wang <873869027@qq.com>
2018-10-05 12:54:29 +0800

    Update Solution.java

--- Java/_014_Longest_Common_Prefix/Solution.java
+++ Java/_014_Longest_Common_Prefix/Solution.java
@@ -3,12 +3,12 @@ package _014_Longest_Common_Prefix;
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
         int min = 0;
-	while (strs.length > 0) {
+	 while (strs.length > 0) {
 	    for (String s : strs) 
 		if (s.length() == min || s.charAt(min) != strs[0].charAt(min)) 
 		    return strs[0].substring(0, min);
 	    min++;
-	}
-	return new String("");
+	 }
+	 return new String("");
     }
 }
