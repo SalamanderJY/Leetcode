266ce8c6f434a1344d730ace9522b4bcb326f997
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-07 14:30:48 +0800

    Update Solution_021.cpp

--- Cplusplus/_021_Merge_Two_Sorted_Lists/Solution_021.cpp
+++ Cplusplus/_021_Merge_Two_Sorted_Lists/Solution_021.cpp
@@ -10,24 +10,22 @@
 #include <iostream>
 
 struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		ListNode dummy(0);
-		ListNode *tail = &dummy;
-		while (l1 && l2) {
-			ListNode* (&node) = l1->val < l2->val ? l1 : l2;
-			tail = tail->next = node;
-			node = node->next;
-		}
-		tail->next = l1 ? l1 : l2;
-		return dummy.next;
-        
-        
+	ListNode dummy(0);
+	ListNode *tail = &dummy;
+	while (l1 && l2) {
+	    ListNode* (&node) = l1->val < l2->val ? l1 : l2;
+	    tail = tail->next = node;
+	    node = node->next;
+	}
+	tail->next = l1 ? l1 : l2;
+	return dummy.next;  
     }
 };
