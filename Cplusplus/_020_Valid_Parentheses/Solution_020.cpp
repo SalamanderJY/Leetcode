fff8fac1d425497b764de89f179fcf207f4c080c
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-07 14:26:44 +0800

    Update Solution_020.cpp

--- Cplusplus/_020_Valid_Parentheses/Solution_020.cpp
+++ Cplusplus/_020_Valid_Parentheses/Solution_020.cpp
@@ -6,26 +6,22 @@ using namespace std;
 
 class Solution {
 public:
-	bool isValid(string s) {
-
-		stack<char> stack;
-		map<char, char> map = { { ']', '[' }, { ')', '(' }, { '}', '{' } };
-		for (int i = 0; i < s.length(); i++)
-		{
-			if (s.at(i) == '(' || s.at(i) == '[' || s.at(i) == '{')
-			{
-				stack.push(s.at(i));
-			}
-			else
-			{
-				if (stack.empty())
-					return false;
-				char top = stack.top();
-				if (top != map[s.at(i)])
-					return false;
-				stack.pop();
-			}
-		}
-		return stack.empty();
+    bool isValid(string s) {
+	stack<char> stack;
+	map<char, char> map = { { ']', '[' }, { ')', '(' }, { '}', '{' } };
+	for (int i = 0; i < s.length(); i++) {
+	    if (s.at(i) == '(' || s.at(i) == '[' || s.at(i) == '{') {
+		stack.push(s.at(i));
+	    }
+	    else {
+		if (stack.empty())
+		    return false;
+		char top = stack.top();
+		if (top != map[s.at(i)])
+		    return false;
+		stack.pop();
+	    }
 	}
+	return stack.empty();
+    }
 };
