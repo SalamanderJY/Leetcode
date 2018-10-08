b47970709bc0d95630d41ba2b60e436c2c67a53a
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2018-10-08 12:52:51 +0800

    023 Merge k Sorted Lists

--- Java/_023_Merge_k_Sorted_Lists/Solution.java
+++ Java/_023_Merge_k_Sorted_Lists/Solution.java
@@ -1,30 +1,27 @@
-package _23_Merge_k_Sorted_Lists;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
-
-
-public class MergeList {
+public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         Queue<ListNode> heap = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
-			@Override
-			public int compare(ListNode o1, ListNode o2) {
-				// TODO Auto-generated method stub
-				return o1.val - o2.val;
-			}	
-		});
-        
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+	        // TODO Auto-generated method stub
+	        return o1.val - o2.val;
+            }	
+        });
         ListNode result = new ListNode(0);
         ListNode temp = result;
-        
         for (ListNode listNode : lists) {
-			if(listNode != null) {
-				heap.add(listNode);
-			}
-		}
-        
+	    if(listNode != null)
+		heap.add(listNode);
+	}   
         while (!heap.isEmpty()) {
             ListNode cur = heap.poll();
             temp.next = cur;
@@ -32,14 +29,7 @@ public class MergeList {
                 heap.add(cur.next);
             }
             temp = temp.next;
-        }
-               
+        }         
         return result.next;
     }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
 }
