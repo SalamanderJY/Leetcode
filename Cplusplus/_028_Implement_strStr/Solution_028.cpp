9dc0ddd365a6abe24c3906b8f54d0b8e408d98f9
Jianyu.Wang <873869027@qq.com>
2018-10-10 11:12:50 +0800

    Update Solution_028.cpp

--- Cplusplus/_028_Implement_strStr/Solution_028.cpp
+++ Cplusplus/_028_Implement_strStr/Solution_028.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-	int strStr(string haystack, string needle) {
-        
-		if (needle.size() == 0)
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0)
 			return 0;
 
 		int i = 0, j = 0;
@@ -24,26 +23,24 @@ public:
 
 	}
 
-	// KMP find slide distance of needle.
-	vector<int> findNext(string& needle) {
-		int n = needle.length();
-		vector<int> next(n, 0);
-		int k = -1, j = 0;
-		next[0] = -1;
-
-		while (j < n - 1) {
-			if (k == -1 || needle.at(k) == needle.at(j)) 
-            {
-				k++;
-				j++;
-				if (needle.at(k) == needle.at(j))           
-					next[j] = next[k];
-				else
-					next[j] = k;
-			}
-			else 
-				k = next[k];
-		}
-		return next;
+    // KMP find slide distance of needle.
+    vector<int> findNext(string& needle) {
+	int n = needle.length();
+	vector<int> next(n, 0);
+	int k = -1, j = 0;
+	next[0] = -1;
+        while (j < n - 1) {
+	    if (k == -1 || needle.at(k) == needle.at(j)) {
+		k++;
+		j++;
+		if (needle.at(k) == needle.at(j))           
+		    next[j] = next[k];
+		else
+		    next[j] = k;
+	    }
+	    else 
+		k = next[k];
 	}
+	return next;
+    }
 };
