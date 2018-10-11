bb1aca04c2b501e773718de1071be0037aff06fa
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-11 11:41:08 +0800

    Update Solution_031.cpp

--- Cplusplus/_031_Next_Permutation/Solution_031.cpp
+++ Cplusplus/_031_Next_Permutation/Solution_031.cpp
@@ -8,10 +8,12 @@ public:
         // reverse all the numbers after violated number
         reverse(nums.begin() + i + 1, nums.end());
         // if violated number not found, because we have reversed the whole array, then we are done!
-        if (i == -1) return;
+        if (i == -1) 
+            return ;
         // else binary search find the first number larger than the violated number
         auto itr = upper_bound(nums.begin() + i + 1, nums.end(), nums[i]);
         // swap them, done!
         swap(nums[i], *itr);
+        return ;
     }
 };
