5fd13fbc66c70134e899d7ff9cf6b7a254ea023e
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-11 11:34:48 +0800

    Update Solution.java

--- Java/_031_Next_Permutation/Solution.java
+++ Java/_031_Next_Permutation/Solution.java
@@ -5,16 +5,14 @@ class Solution {
             if (nums[i] < nums[i + 1]) 
                 break;
         }    
-        for (int j = i + 1; j < (nums.length + i + 1) / 2; j++)
-        {
+        for (int j = i + 1; j < (nums.length + i + 1) / 2; j++) {
             int temp = nums[j];
             nums[j] = nums[nums.length + i - j];
             nums[nums.length + i - j] = temp;
         }
         if (i == -1) return;
         for (int j = i + 1; j < nums.length; j++)
-            if (nums[j] > nums[i]) 
-            {
+            if (nums[j] > nums[i]) {
                 int temp = nums[j];
                 nums[j] = nums[i];
                 nums[i] = temp;
