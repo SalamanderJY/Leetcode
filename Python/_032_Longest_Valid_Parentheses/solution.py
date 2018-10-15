15da7db1ffcf68784a777aa621942b362c263698
ubuntu <873869027@qq.com>
2018-10-15 13:05:38 +0800

    032 Longest Valid Parentheses

---
+++ Python/_032_Longest_Valid_Parentheses/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) < 2:
+            return 0
+        max_value = 0
+        dp = [0] * len(s)
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] == '(':
+                j = i + 1 + dp[i + 1]
+                if j < len(s) and s[j] == ')':
+                    dp[i] = dp[i + 1] + 2
+                    if j + 1 < len(s):
+                        dp[i] += dp[j + 1]
+                if dp[i] > max_value:
+                    max_value = dp[i]
+        return max_value
+
