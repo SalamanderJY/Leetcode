89b7ba2b86b4d24eef41273341e9363da3919e30
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-15 13:12:35 +0800

    Update Solution_033.cpp

--- Cplusplus/_033_Search_in_Rotated_Sorted_Array/Solution_033.cpp
+++ Cplusplus/_033_Search_in_Rotated_Sorted_Array/Solution_033.cpp
@@ -5,28 +5,27 @@ public:
             return -1;
         int left = 0;
         int right = nums.size() - 1;
-        while(left + 1 < right)
-        {
+        while (left + 1 < right) {
             if (nums[left] == target) return left;
             if (nums[right] == target) return right;
             int mid = left + (right - left) / 2;
             if (nums[mid] == target)
                 return mid;
-            if (nums[mid] < target)
-            {
+            if (nums[mid] < target) {
                 if ((target < nums[right]) || (target > nums[right] && nums[mid] > nums[right]))
                     left = mid;
                 else right = mid;
             }
-            else if (nums[mid] > target)
-            {
+            else if (nums[mid] > target) {
                 if ((target > nums[left]) || (target < nums[left] && nums[mid] < nums[left]))
                     right = mid;
                 else left = mid;
             }
         }
-        if (nums[left] == target) return left;
-        if (nums[right] == target) return right;
+        if (nums[left] == target) 
+            return left;
+        if (nums[right] == target) 
+            return right;
         return -1;
     }
 };
