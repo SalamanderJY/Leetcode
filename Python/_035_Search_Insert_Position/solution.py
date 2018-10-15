fef242b740512ff6027ce1398ce9e16a910cffc9
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-15 13:24:20 +0800

    Update solution.py

--- Python/_035_Search_Insert_Position/solution.py
+++ Python/_035_Search_Insert_Position/solution.py
@@ -7,12 +7,10 @@ class Solution:
         """
         low = 0
         high = len(nums) - 1
-
         while low <= high: 
             mid = low + (high - low) // 2
             if nums[mid] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-
         return low
