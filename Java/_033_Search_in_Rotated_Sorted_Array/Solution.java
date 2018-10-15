c4068b4273c4eefaf56266580fb5dfaced3b0e67
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-15 13:13:42 +0800

    Update Solution.java

--- Java/_033_Search_in_Rotated_Sorted_Array/Solution.java
+++ Java/_033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -4,28 +4,29 @@ class Solution {
             return -1;
         int left = 0;
         int right = nums.length - 1;
-        while(left + 1 < right)
-        {
-            if (nums[left] == target) return left;
-            if (nums[right] == target) return right;
+        while (left + 1 < right) {
+            if (nums[left] == target) 
+                return left;
+            if (nums[right] == target) 
+                return right;
             int mid = left + (right - left) / 2;
             if (nums[mid] == target)
                 return mid;
-            if (nums[mid] < target)
-            {
+            if (nums[mid] < target) {
                 if ((target < nums[right]) || (target > nums[right] && nums[mid] > nums[right]))
                     left = mid;
                 else right = mid;
             }
-            else if (nums[mid] > target)
-            {
+            else if (nums[mid] > target) {
                 if ((target > nums[left]) || (target < nums[left] && nums[mid] < nums[left]))
                     right = mid;
                 else left = mid;
             }
         }
-        if (nums[left] == target) return left;
-        if (nums[right] == target) return right;
+        if (nums[left] == target) 
+            return left;
+        if (nums[right] == target) 
+            return right;
         return -1;
     }
 }
