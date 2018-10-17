d828e3ca11496091575657c5eb8e07708371ffa3
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-17 22:24:46 +0800

    Update Solution_038.cpp

--- Cplusplus/_038_Count_and_Say/Solution_038.cpp
+++ Cplusplus/_038_Count_and_Say/Solution_038.cpp
@@ -4,7 +4,7 @@ public:
         if (n < 1)
 	    return NULL;
         string result = "1";
-	for (int i = 2; i <= n; i++)
+	 for (int i = 2; i <= n; i++)
 	    result = countAndSay(result);
         return result;
     }
