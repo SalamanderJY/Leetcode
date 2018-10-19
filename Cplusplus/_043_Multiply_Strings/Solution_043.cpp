bc5fdc1be1b67e97fca1038598eb1d412947782d
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-19 15:51:17 +0800

    Update Solution_043.cpp

--- Cplusplus/_043_Multiply_Strings/Solution_043.cpp
+++ Cplusplus/_043_Multiply_Strings/Solution_043.cpp
@@ -3,7 +3,7 @@ public:
     string multiply(string num1, string num2) {
         if (num1.size() == 0 || num2.size() == 0 || num1 == "0" || num2 == "0")
             return to_string(0);
-        int *res = new int[num1.size() + num2.size()];
+        int* res = new int[num1.size() + num2.size()];
         int length = num1.size() + num2.size();
         for (int i = 0; i < length; i++)
             res[i] = 0;
@@ -13,7 +13,8 @@ public:
             for (int j = num2.size() - 1; j >= 0; --j) {
                 int mul = res[idx] + (num1.at(i) - '0') * (num2.at(j) - '0') + carry;
                 carry = mul / 10;
-                res[idx--] = mul % 10;
+                res[idx] = mul % 10;
+                
             }
             res[idx] += carry;
         }
