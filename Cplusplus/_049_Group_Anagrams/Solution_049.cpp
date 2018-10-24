902ca85a0a5a840c21ba65bb1f6d4a2279b2eccb
Jianyu.Wang <wangjianyu@hit.edu.cn>
2018-10-24 17:08:15 +0800

    Update Solution_049.cpp

--- Cplusplus/_049_Group_Anagrams/Solution_049.cpp
+++ Cplusplus/_049_Group_Anagrams/Solution_049.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        if (strs.size() == 0) return ans;
+        if (strs.size() == 0) 
+            return ans;
         unordered_map<string, vector<string>> map;
         for (auto it : strs) {
             string value = it;
