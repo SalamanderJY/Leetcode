8c7187d90a49a0dec1a3f7c321db7001c2915b26
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2019-12-31 08:33:30 +0800

    change tab to space

--- Cplusplus/_001_Two_Sum/Solution_001.cpp
+++ Cplusplus/_001_Two_Sum/Solution_001.cpp
@@ -9,7 +9,6 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
         vector<int> result;
-
         for (int i = 0; i < nums.size(); i++) {
             int numToFind = target - nums[i];
             if (hash.find(numToFind) != hash.end()) {
