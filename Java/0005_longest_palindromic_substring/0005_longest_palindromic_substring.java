d6b6a6b8ce2afb2882328d53d220e28fc1917871
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2020-02-01 21:37:38 +0800

    0005 0005_longest_palindromic_substring

---
+++ Java/0005_longest_palindromic_substring/0005_longest_palindromic_substring.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length() == 0)
+            return s;
+        boolean[][] dp = new boolean[1000][1000];
+        int start = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1]))
+                    dp[j][i] = true;
+                if (dp[j][i] && max < i - j + 1) {
+                    max = i - j + 1;
+                    start = j;
+                }
+            }
+        }
+        return s.substring(start, start + max);
+    }
+}
