ccdad77053f2c7377205eeee50ef5d2b9b9d27b6
Jianyu.Wang <873869027@qq.com>
2020-02-16 19:18:50 +0800

    0008 atoi

---
+++ C/0008_string_to_integer_atoi/0008_string_to_integer_atoi.c
@@ -0,0 +1,25 @@
+int myAtoi(char* str) {
+    if(strlen(str) == 0) 
+        return 0;       
+    int result = 0;
+    int index = 0;
+    int flag = 1;      
+    while (str[index] == ' ')
+        index++; 
+    if (str[index] == '-') {
+        flag = -1;
+        index++;
+    }       
+    else if (str[index] == '+') {
+        flag = 1;
+        index++;
+    }       
+    for (int i = index; i < strlen(str); i++) {
+        if (str[i] < '0' || str[i] > '9')
+            break;
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10))
+            return flag == -1 ? INT_MIN : INT_MAX;
+        result = result * 10 + (str[i] - '0');
+    }     
+    return result * flag;
+}
