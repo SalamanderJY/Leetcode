fa7507c2cfc2e217ec85c1c37d06a09a1c706531
Jianyu.Wang <873869027@qq.com>
2020-04-25 18:48:38 +0800

    0021_merge_two_sorted_lists

--- Cplusplus/0021_merge_two_sorted_lists/0021_merge_two_sorted_lists.cpp
+++ Cplusplus/0021_merge_two_sorted_lists/0021_merge_two_sorted_lists.cpp
@@ -18,14 +18,14 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-	ListNode dummy(0);
-	ListNode *tail = &dummy;
-	while (l1 && l2) {
-	    ListNode* (&node) = l1->val < l2->val ? l1 : l2;
-	    tail = tail->next = node;
-	    node = node->next;
-	}
-	tail->next = l1 ? l1 : l2;
-	return dummy.next;  
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+        while (l1 && l2) {
+            ListNode* (&node) = l1->val < l2->val ? l1 : l2;
+            tail = tail->next = node;
+            node = node->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;  
     }
 };
