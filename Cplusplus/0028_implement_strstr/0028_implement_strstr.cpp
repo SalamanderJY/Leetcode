b9391176c0a75dad1c5c72fb7dffb7556fa725c5
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2020-05-17 22:11:43 +0800

    0028_implement_strstr

---
+++ Cplusplus/0028_implement_strstr/0028_implement_strstr.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0)
+            return 0;
+        int i = 0, j = 0;
+        vector<int> next = findNext(needle);
+        while (i < haystack.size() && j != needle.size()) {
+            if (j == -1 || haystack.at(i) == needle.at(j)) {
+                i++;
+                j++;
+            }
+            else   
+                j = next[j];
+        }
+        if (j == needle.size())
+            return i - j;
+        return -1;
+    }
+
+    // KMP find slide distance of needle.
+    vector<int> findNext(string& needle) {
+        int n = needle.length();
+        vector<int> next(n, 0);
+        int k = -1, j = 0;
+        next[0] = -1;
+        while (j < n - 1) {
+            if (k == -1 || needle.at(k) == needle.at(j)) {
+                k++;
+                j++;
+                next[j] = k;
+            }
+            else 
+                k = next[k];
+        }
+        return next;
+    }
+};
