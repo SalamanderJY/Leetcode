17da2450bf827ed7ec987276da465fabec5e3a22
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2021-04-06 22:51:10 +0800

    0070_climbing_stairs

--- Cplusplus/0070_climbing_stairs/0070_climbing_stairs.cpp
+++ Cplusplus/0070_climbing_stairs/0070_climbing_stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
-    public int climbStairs(int n) {
-        if(n == 0)
+public:
+    int climbStairs(int n) {
+        if (n == 0)
             return 0;
         
-        int[] dp = new int[n + 2];
+        int* dp = new int[n + 2];
         dp[0] = 1;
         dp[1] = 2;
         dp[2] = 3;
         for (int i = 3; i < n; i++)
             dp[i] = dp[i - 1] + dp[i - 2];
-        
-        return dp[n - 1];   
+        int res = dp[n - 1];
+        delete[] dp;
+        return res;       
     }
-}
+};
